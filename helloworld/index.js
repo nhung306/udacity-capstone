c76035ed42686cb305b0887075a56a8983da9976
EC2 Default User <ec2-user@ip-172-31-65-109.ec2.internal>
2022-10-01 05:42:40 +0000

    update

--- helloworld/index.js
+++ helloworld/index.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Worl, my name is Minh Duong v2\n'
+  const msg = 'Hello World, my name is Minh Duong v1\n'
   res.end(msg);
 });
 
